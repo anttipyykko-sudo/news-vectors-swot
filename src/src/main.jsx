7358c273fccca702b40c5ac53d01fccbc5cfe5b2
anttipyykko-sudo <antti.pyykko@protonmail.com>
2025-11-08 23:03:23 +0200

    Create main.jsx

---
+++ src/src/main.jsx
@@ -0,0 +1,4 @@
+import React from 'https://cdn.skypack.dev/react@18';
+import ReactDOM from 'https://cdn.skypack.dev/react-dom@18';
+import App from './App.jsx';
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
